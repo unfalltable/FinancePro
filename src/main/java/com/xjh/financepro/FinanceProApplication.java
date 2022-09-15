46d6d5ed66f0f45b9884538bb423f265ad9e8f44
xjh <1563077843@qq.com>
2022-09-15 11:54:37 +0800

    Initial commit

---
+++ src/main/java/com/xjh/financepro/FinanceProApplication.java
@@ -0,0 +1,13 @@
+package com.xjh.financepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceProApplication.class, args);
+    }
+
+}
